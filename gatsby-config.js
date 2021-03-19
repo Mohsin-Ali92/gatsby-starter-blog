b41e345e7bd22ee8dbd6b3025066821e156d2b2a
Mohsin-Ali92 <mohsin93790@gmail.com>
2021-03-19 18:20:01 +0530

    Changed the text on the gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,11 @@ module.exports = {
         purpose: "maskable",
       },
     },
+    `gatsby-plugin-gatsby-cloud`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+
+    `gatsby-plugin-offline`,
   ],
 }
