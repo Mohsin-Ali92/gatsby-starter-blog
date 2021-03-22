0a722bcaddf06ffab47765dbe33d661e1a881a5f
Mohsin-Ali92 <mohsin93790@gmail.com>
2021-03-22 12:46:09 +0530

    Changed the text on gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Flutter Is About To Win Over the Web`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@mohsin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
